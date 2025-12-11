f196d35af44bf9c39109f24921004ce2415f1b0d
Richard M <36093669+itsrichardmai@users.noreply.github.com>
2025-12-10 16:19:57 -0800

    Add price_change_percentage_24h to type

--- types/crypto.ts
+++ types/crypto.ts
@@ -4,6 +4,7 @@ export interface CryptoAsset {
     name: string;
     image: string;
     current_price: number;
+    price_change_percentage_24h: number;  // ADD THIS LINE
     market_cap: number;
     market_cap_rank: number;
     total_volume: number;
